06c2d0dd35a4e90e54a48e4c30f22d1f49a971dc
dunglt94 <tuandung9594@gmail.com>
2025-01-23 14:27:45 +0700

    add loginform component with validate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css"
 import SignupForm from "./components/SignupForm";
+import LoginForm from "./components/LoginForm";
 
 export default function App() {
     return (
-        <div style={{padding: '2rem'}}>
-            <SignupForm/>
-        </div>
+        <>
+            <div style={{padding: '2rem'}}>
+                <SignupForm />
+            </div>
+            <div style={{padding: '2rem'}}>
+                <LoginForm />
+            </div>
+        </>
+
     )
 }
