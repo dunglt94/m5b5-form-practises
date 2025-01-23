8e12f2790f50afa122545bcd19852d6faf752312
dunglt94 <tuandung9594@gmail.com>
2025-01-23 14:06:57 +0700

    first add

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css"
+import SignupForm from "./components/SignupForm";
+
+export default function App() {
+    return (
+        <div style={{padding: '2rem'}}>
+            <SignupForm/>
+        </div>
+    )
+}
